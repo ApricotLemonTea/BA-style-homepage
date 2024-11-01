22dae1873cab7bd566e9a9168a0c115c2fd3027f
杏仁レモンティー <apricotlemontea@gmail.com>
2024-11-01 20:47:47 +0900

    patch note动画效果完成

--- src/components/PatchNote.vue
+++ src/components/PatchNote.vue
@@ -16,6 +16,7 @@ defineExpose({ open })
            ok-text="閉じる"
            hide-cancel
            class="patch-note-modal"
+           modal-animation-name="re-zoom-modal"
   >
     <div class="patch-note-container">
       123456789022345678901234567890223456789012345678902234567890
@@ -31,13 +32,28 @@ defineExpose({ open })
     width: 70vw;
   }
 }
-.patch-note-modal .patch-note-modal-enter-from,
-.patch-note-modal .patch-note-modal-appear-from{
-  transform: scale(0.5) translateY(0) !important;
-  opacity: 1 !important;
+
+.re-zoom-modal-enter-from,.re-zoom-modal-appear-from {
+  transform: scale(0.5) translateY(0);
+  opacity: 0
 }
-.patch-note-modal .patch-note-modal-enter-active,
-.patch-note-modal .patch-note-modal-appear-active {
+.re-zoom-modal-enter-to,.re-zoom-modal-appear-to {
+  transform: scale(1) translateY(0);
+  opacity: 1
+}
+.re-zoom-modal-enter-active,.re-zoom-modal-appear-active {
   transition: opacity .4s cubic-bezier(.3,1.3,.3,1),transform .4s cubic-bezier(.3,1,.3,1) !important;
+}
+
+.re-zoom-modal-leave-from {
+  transform: scale(1) translateY(0); /* 从正常大小和位置开始 */
+  opacity: 1; /* 完全不透明 */
+}
+.re-zoom-modal-leave-to {
+  transform: scale(0.5) translateY(0); /* 缩小 */
+  opacity: 0; /* 变得透明 */
+}
+.re-zoom-modal-leave-active {
+  transition: opacity .4s cubic-bezier(.3, 1.3, .3, 1), transform .4s cubic-bezier(.3, 1, .3, 1) !important; /* 应用过渡效果 */
 }
 </style>
