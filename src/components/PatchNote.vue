555ca4d2ad70c3b1e1e1daf2b2ca98b490056a37
杏仁レモンティー <apricotlemontea@gmail.com>
2024-11-02 18:09:21 +0900

    2024-11-02 patch note

--- src/components/PatchNote.vue
+++ src/components/PatchNote.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref } from 'vue'
+import patchNote from '/_patch_note.json'
 
 const dialogVisible = ref(false)
 
@@ -18,18 +19,34 @@ defineExpose({ open })
            modal-animation-name="no-slide-zoom-modal"
   >
     <div class="patch-note-container">
-      <div>
-        <h2>2024-11-01　β版公開</h2>
-        <a-divider :size="3"/>
+      <div v-for="note in patchNote.patchNoteList">
+        <h1>{{note.title}}</h1>
+        <div class="patch-note-text-block">
+          <p v-for="item in note.contents" class="patch-note-text">
+            ・{{item}}
+          </p>
+        </div>
+        <a-divider :size="3" :margin="50"/>
       </div>
     </div>
+
   </a-modal>
 </template>
 
 <style>
 .patch-note-container {
   height: 60vh;
-  width: 70vw;
+  width: auto;
+  min-width: 50vw;
   color: #003153;
+}
+
+.patch-note-text-block {
+  text-align: left;
+  padding: 0 7vw;
+}
+
+.patch-note-text {
+  font-size: 20px;
 }
 </style>
