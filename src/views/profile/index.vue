ebbf71e3137465849723e5c56f1e1c4ce6864404
ApricotLemonTea <apricotlemontea@gmail.com>
2024-11-07 14:43:34 +0900

    profile页面布局

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -3,11 +3,41 @@
 </script>
 
 <template>
-  <div>
-    プロフィール画面
+  <div class="profile-container">
+    <div class="id-card"></div>
+    <div class="button-block">
+      <div class="button"></div>
+      <div class="button"></div>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.profile-container {
+  background-color: #c0ffbb;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  align-items: center;
 
+  .id-card {
+    background-color: white;
+    width: 60vw;
+    height: 70vh;
+    margin-left: 7vw;
+  }
+
+  .button-block {
+    background-color: #ffbeaf;
+    height: 70vh;
+    margin-left: 7vw;
+
+    .button {
+      background-color: #fcd3ff;
+      width: 20vw;
+      height: 10vh;
+      margin-top: 3vh;
+    }
+  }
+}
 </style>
