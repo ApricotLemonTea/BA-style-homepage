0ed2c646338835e01198db67d0b68c1395683ded
ApricotLemonTea <apricotlemontea@gmail.com>
2024-11-07 15:20:22 +0900

    按钮样式调整

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -6,8 +6,12 @@
   <div class="profile-container">
     <div class="id-card"></div>
     <div class="button-block blue-text-color">
-      <div class="button">プロフィール</div>
-      <div class="button">オリキャラ</div>
+      <div class="button">
+        <p class="button-text">プロフィール</p>
+      </div>
+      <div class="button">
+        <p class="button-text">オリキャラ</p>
+      </div>
     </div>
   </div>
 </template>
@@ -37,11 +41,15 @@
       margin-top: 3vh;
       border-radius: 10px;
       display: flex;
-      justify-content: center;
+      justify-content: start;
       align-items: center;
       font-size: 3.5vh;
       box-shadow: 3px 3px 7px rgba(0, 0, 0, 0.2);
       border: 1px solid #b6c0c8;
+
+      .button-text {
+        margin-left: 20px;
+      }
     }
     .button:active {
       transform: scale(0.95);
