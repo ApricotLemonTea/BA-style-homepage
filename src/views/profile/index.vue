2706dadaaf0cb2241ee3af9df8c4f76baf2c29bb
ApricotLemonTea <apricotlemontea@gmail.com>
2024-11-07 14:57:51 +0900

    按钮样式

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -5,16 +5,15 @@
 <template>
   <div class="profile-container">
     <div class="id-card"></div>
-    <div class="button-block">
-      <div class="button"></div>
-      <div class="button"></div>
+    <div class="button-block blue-text-color">
+      <div class="button">プロフィール</div>
+      <div class="button">オリキャラ</div>
     </div>
   </div>
 </template>
 
 <style scoped>
 .profile-container {
-  background-color: #c0ffbb;
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -28,15 +27,22 @@
   }
 
   .button-block {
-    background-color: #ffbeaf;
     height: 70vh;
     margin-left: 7vw;
 
     .button {
-      background-color: #fcd3ff;
+      background-color: white;
       width: 20vw;
-      height: 10vh;
+      height: 8vh;
       margin-top: 3vh;
+      border-radius: 10px;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-size: 3.5vh;
+    }
+    .button:active {
+      transform: scale(0.95);
     }
   }
 }
