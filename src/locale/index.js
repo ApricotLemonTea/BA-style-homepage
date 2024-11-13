cc6e79bc9becb24515e4c7ca6cc846fd5b79082d
ApricotLemonTea <apricotlemontea@gmail.com>
2024-11-13 13:35:20 +0900

    增加中文配置

--- src/locale/index.js
+++ src/locale/index.js
@@ -1,11 +1,13 @@
 import { createI18n } from "vue-i18n"
 import ja from './ja'
+import zh from './zh'
 
 const i18n = createI18n({
   locale: 'ja',
   fallbackLocale: 'ja',
   messages: {
-    ja
+    ja,
+    zh
   }
 })
 
