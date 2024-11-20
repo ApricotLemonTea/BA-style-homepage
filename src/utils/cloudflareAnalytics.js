107a155214c570dbbce47322c4e50bec9e52b5b8
ApricotLemonTea <apricotlemontea@gmail.com>
2024-11-20 12:26:56 +0900

    优化graphql请求参数的字符串写法

--- src/utils/cloudflareAnalytics.js
+++ src/utils/cloudflareAnalytics.js
@@ -1,27 +1,43 @@
 import axios from 'axios'
+import { getFormattedDate } from '@/utils/commonFunctions'
 
 const apiToken = import.meta.env.VITE_CLOUDFLARE_API_TOKEN
 const zoneId = import.meta.env.VITE_CLOUDFLARE_ZONE_ID
 
 /**
- * cloudflare api统计从2024-11-01至今的page view总和
- * @returns { Promise<number> } page view总和
+ * cloudflare api统计从2024-11-01至今的访问量总和
+ * @returns { Promise<number> } 访问量总和
  */
-async function getAccessAnalytics() {
+const getAccessAnalytics = async () => {
   const headers = {
-    Authorization: "Bearer " + apiToken,
+    Authorization: `Bearer ${apiToken}`
   }
   const data = {
-    query: "{" +
-      "viewer {" +
-        "zones(filter: {zoneTag:" + zoneId + "})" +
-          "{httpRequests1dGroups (" +
-          "limit: 9999," +
-          "filter: {date_geq:\"2024-11-01\",date_leq:\"" + formatDate(new Date()) + "\"}," +
-          "orderBy: [date_DESC])" +
-          "{sum {bytes, cachedBytes, cachedRequests, encryptedBytes, encryptedRequests, pageViews, requests, threats}" +
-        "dimensions {date}" +
-      "}}}}",
+    query: `{
+      viewer {
+        zones(filter: { zoneTag: "${zoneId}" }) {
+          httpRequests1dGroups(
+            limit: 9999,
+            filter: { date_geq: "2024-11-01", date_leq: "${getFormattedDate(new Date())}" },
+            orderBy: [date_DESC]
+          ) {
+            sum {
+              bytes
+              cachedBytes
+              cachedRequests
+              encryptedBytes
+              encryptedRequests
+              pageViews
+              requests
+              threats
+            }
+            dimensions {
+              date
+            }
+          }
+        }
+      }
+    }`,
     variables: {}
   }
   const res = await axios.post("/client/v4/graphql", data, { headers })
@@ -32,16 +48,6 @@ async function getAccessAnalytics() {
   }
 
   return result
-}
-
-/**
- * 格式化日期为 yyyy-MM-DD
- */
-function formatDate(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${year}-${month}-${day}`;
 }
 
 export default getAccessAnalytics
