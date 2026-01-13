c0323f9c070d57cd84161d280c8cbb4b80429d77
杏仁レモンティー <apricotlemontea@gmail.com>
2026-01-13 22:09:19 +0900

    console warn修复

--- src/components/SpotifyPlayer.vue
+++ src/components/SpotifyPlayer.vue
@@ -20,7 +20,6 @@ const isShowPlayer = computed(() => {
         width="100%"
         height="152"
         frameBorder="0"
-        allowfullscreen=""
         allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
         loading="lazy"
       ></iframe>
