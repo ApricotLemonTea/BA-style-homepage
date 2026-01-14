92f5782c5b5c08a05291a8330223f1a527360623
杏仁レモンティー <apricotlemontea@gmail.com>
2026-01-14 12:45:01 +0900

    页面更新逻辑

--- src/main.js
+++ src/main.js
@@ -15,6 +15,7 @@ import * as PIXI from 'pixi.js'
 import hina_swimsuit from '/l2d/hina_swimsuit/CH0063_home.skel?url'
 // import hina_bgm from '/l2d/hina_swimsuit/Theme_21.mp3'
 import aris from '/l2d/aris/Aris_home.skel?url'
+import { registerSW } from 'virtual:pwa-register'
 
 // import { css as cssJa } from './assets/font/Natsuzemi-Maru-Gothic.ttf';
 // import { css as cssZh } from './assets/font/BlueakaBeta2GBK-DemiBold.ttf';
@@ -33,35 +34,18 @@ app.use(i18n)
 app.mount('#app')
 
 // i18n t函数
-const t = i18n.global.t
+// const t = i18n.global.t
 
 /**
- * 页面需要更新时的弹窗提示
+ * 有更新时下次进入页面时更新
  */
-// if ('serviceWorker' in navigator) {
-//   const updateSW = registerSW({
-//     onNeedRefresh() {
-//       Modal.open({
-//         title: t('notice.メッセージ'),
-//         content: () => [
-//           h(
-//             'p',
-//             { class: 'blue-text-color' },
-//             t('notice.アップデートがあります、ページを再起動してください。')
-//           )
-//         ],
-//         okText: t('はい'),
-//         hideCancel: true,
-//         closable: false,
-//         maskClosable: false,
-//         escToClose: false,
-//         onOk: () => {
-//           updateSW(true)
-//         }
-//       })
-//     }
-//   })
-// }
+if ('serviceWorker' in navigator) {
+  const updateSW = registerSW({
+    onNeedRefresh() {
+      updateSW(false)
+    }
+  })
+}
 
 window.l2d_complete = false
 
