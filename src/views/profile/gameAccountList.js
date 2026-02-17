9a434948eeef423239be9f676c9dccea4aa57833
杏仁レモンティー <apricotlemontea@gmail.com>
2026-02-17 12:33:42 +0900

    游戏账号截图更新

--- src/views/profile/gameAccountList.js
+++ src/views/profile/gameAccountList.js
@@ -3,7 +3,7 @@ export const gameAccountList = [
     imgUrl: '/profile/ba.png',
     server: 'ブルーアーカイブ / 日本版',
     characterName: '杏仁レモンティー',
-    time: '(2026-01)'
+    time: '(2026-02)'
   },
   {
     imgUrl: '/profile/ff14.png',
@@ -15,12 +15,12 @@ export const gameAccountList = [
     imgUrl: '/profile/dnf.png',
     server: '地下城与勇士 / 跨五 / 上海一区',
     characterName: '杏仁柠檬茶',
-    time: '(2026-01)'
+    time: '(2026-02)'
   },
   {
     imgUrl: '/profile/steam.png',
     server: 'Steam',
     characterName: '杏仁柠檬茶',
-    time: '(2025-12)'
+    time: '(2026-02)'
   }
 ]
