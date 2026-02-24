4a6aeea463396b089f424a8bef2884914a914a84
杏仁レモンティー <apricotlemontea@gmail.com>
2026-02-24 23:23:17 +0900

    mission更新

--- src/views/profile/gameAccountList.js
+++ src/views/profile/gameAccountList.js
@@ -1,17 +1,5 @@
 export const gameAccountList = [
   {
-    imgUrl: '/profile/ba.png',
-    server: 'ブルーアーカイブ / 日本版',
-    characterName: '杏仁レモンティー',
-    time: '(2026-02)'
-  },
-  {
-    imgUrl: '/profile/ff14.png',
-    server: 'FINAL FANTASY XIV / Mana / Asura',
-    characterName: 'Apricot Lemontea',
-    time: '(2026-02)'
-  },
-  {
     imgUrl: '/profile/dnf.png',
     server: '地下城与勇士 / 跨五 / 上海一区',
     characterName: '杏仁柠檬茶',
@@ -21,6 +9,18 @@ export const gameAccountList = [
     imgUrl: '/profile/steam.png',
     server: 'Steam',
     characterName: '杏仁柠檬茶',
+    time: '(2026-02)'
+  },
+  {
+    imgUrl: '/profile/ba.png',
+    server: 'ブルーアーカイブ / 日本版',
+    characterName: '杏仁レモンティー',
+    time: '(2026-02)'
+  },
+  {
+    imgUrl: '/profile/ff14.png',
+    server: 'FINAL FANTASY XIV / Mana / Asura',
+    characterName: 'Apricot Lemontea',
     time: '(2026-02)'
   }
 ]
