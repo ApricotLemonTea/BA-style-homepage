edaf0e9448bb2f3f753af70180d813815a8b364f
杏仁レモンティー <apricotlemontea@gmail.com>
2026-04-03 09:55:52 +0900

    不记录自己的访问

--- src/backend/visits.js
+++ src/backend/visits.js
@@ -1,10 +1,15 @@
 import request from '@/utils/request_tmp'
 import { BACKEND_API_URL } from '@/consts/consts'
 
+const isApricotLemonTea = localStorage.getItem('isApricotLemonTea')
+
 export async function countPageVisits(page) {
   request.post(BACKEND_API_URL.COUNT_PAGE_VISITS, null, {
     params: {
       page: page
+    },
+    headers: {
+      isApricotLemonTea: isApricotLemonTea
     }
   })
 }
